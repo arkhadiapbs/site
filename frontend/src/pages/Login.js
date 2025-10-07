cf7591db7eb8aeba9735a37383314318ca330945
saravs858 <sara.silva09@aluno.ifce.edu.br>
2025-10-06 22:06:14 -0300

    Tela de Login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,53 @@
+import React from "react";
+import fundoGamer from '../assets/fundo-gamer.png';
+import "./Login.css";
+
 export default function Login() {
   return (
-    <div>
-      <h1>Página de Login</h1>
-      <p>Aqui vai o formulário de login.</p>
+  
+  <div className="login-container">
+    <div
+      className="login-bg"
+      style={{
+        backgroundImage: `url(${fundoGamer})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+        width: "100vw",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        zIndex: -2
+      }}
+    />
+    <div className="overlay"></div>
+
+  <div className="login-box">
+      
+        <div className="logo-side">
+          <img
+            src="/assets/logo.png" 
+            alt="Logo"
+            className="login-logo"
+          />
+        </div>
+
+        <div className="form-side">
+          <h1 className="login-title">ENTRAR</h1>
+
+          <form className="login-form">
+            <input type="email" placeholder="E-mail" />
+            <input type="password" placeholder="Senha" />
+            <p className="login-text">
+            <a href="#">Crie uma conta</a> para acesso completo, ou entre como{" "}
+            <a href="#">visitante</a>.
+          </p>
+            <button type="submit">AVANÇAR</button>
+          </form>
+
+        </div>
+      </div>
     </div>
   );
 }
